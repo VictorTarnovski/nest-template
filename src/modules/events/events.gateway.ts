707ffc3f89447fe26e4b9777c112fef9948f5b99
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2023-11-20 20:04:57 -0300

    feat: improve typing on events module

--- src/modules/events/events.gateway.ts
+++ src/modules/events/events.gateway.ts
@@ -1,18 +1,19 @@
 import { WebSocketGateway, WebSocketServer } from "@nestjs/websockets"
 import { Server } from "socket.io"
 
-export const Resources = {
-  ingredient: "ingredient",
-  dish: "dish",
-  table: "order",
-  order: "order",
-}
+export const Resources = [
+  "ingredient",
+  "dish",
+  "table",
+  "order"
+]
+
 
 @WebSocketGateway({ namespace: "events" })
 export class EventsGateway {
   @WebSocketServer()
   server: Server
-  sendNewResourceMessage<T>(resourceName: keyof typeof Resources, resource: T) {
+  sendNewResourceMessage<T>(resourceName: (typeof Resources)[number], resource: T) {
     this.server.emit(`new@${resourceName}`, resource)
   }
 }
